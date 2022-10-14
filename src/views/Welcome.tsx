56d32638d94cf9ad335248a205393487d3f1321e
pengmingtao <1491614819@qq.com>
2022-10-14 16:53:48 +0800

    使用滑动事件，hooks,useSwipe.tsx

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -9,26 +9,38 @@ export const Welcome = defineComponent({
     watchEffect(() => {
       console.log(swiping.value, direction.value)
     })
-    return () => <div class={s.wrapper}>
-      <header>
-        <svg>
-          <use xlinkHref='#mangosteen'></use>
-        </svg>
-        <h1>山竹记账</h1>
-      </header>
-      <main class={s.main} ref={main}>
-        <RouterView name="main">
-          {({ Component: X, route: R }: { Component: VNode, route: RouteLocationNormalizedLoaded }) =>
-            <Transition enterFromClass={s.slide_fade_enter_from} enterActiveClass={s.slide_fade_enter_active}
-              leaveToClass={s.slide_fade_leave_to} leaveActiveClass={s.slide_fade_leave_active}>
-              {X}
-            </Transition>
-          }
-        </RouterView>
-      </main>
-      <footer>
-        <RouterView name="footer" />
-      </footer>
-    </div>
+    return () => (
+      <div class={s.wrapper}>
+        <header>
+          <svg>
+            <use xlinkHref="#mangosteen"></use>
+          </svg>
+          <h1>山竹记账</h1>
+        </header>
+        <main class={s.main} ref={main}>
+          <RouterView name="main">
+            {({
+              Component: X,
+              route: R,
+            }: {
+              Component: VNode;
+              route: RouteLocationNormalizedLoaded;
+            }) => (
+              <Transition
+                enterFromClass={s.slide_fade_enter_from}
+                enterActiveClass={s.slide_fade_enter_active}
+                leaveToClass={s.slide_fade_leave_to}
+                leaveActiveClass={s.slide_fade_leave_active}
+              >
+                {X}
+              </Transition>
+            )}
+          </RouterView>
+        </main>
+        <footer>
+          <RouterView name="footer" />
+        </footer>
+      </div>
+    );
   }
 })
