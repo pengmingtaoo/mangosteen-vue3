43c85b22e2077b964902053d7f3668cf26f29920
pengmingtao <1491614819@qq.com>
2022-10-18 17:05:30 +0800

    添加Icon组件

---
+++ src/shared/Icon.tsx
@@ -0,0 +1,19 @@
+import { defineComponent, PropType } from "vue";
+import s from "./Icon.module.scss";
+
+export const Icon = defineComponent({
+  props: {
+    name: {
+      type: String as PropType<
+        "add-bold" | "chart" | "clock" | "cloud" | "mangosteen" | "pig"
+      >,
+    },
+  },
+  setup(props, context) {
+    return () => (
+      <svg class={s.icon}>
+        <use xlinkHref={"#" + props.name}></use>
+      </svg>
+    );
+  },
+});
