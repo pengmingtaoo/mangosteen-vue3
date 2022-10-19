84c03c577e94977b9ced4e62bab607c40f2f525c
pengmingtao <1491614819@qq.com>
2022-10-19 22:19:25 +0800

    创建/items 和/items/create 路由

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,4 +1,5 @@
 import { defineComponent, ref } from "vue";
+import { RouterLink } from "vue-router";
 import { Button } from "../shared/Button";
 import { Center } from "../shared/Center";
 import { FloatButton } from "../shared/FloatButton";
@@ -28,9 +29,13 @@ export const StartPage = defineComponent({
           <Icon name="pig" class={s.icon} />
         </Center>
         <div class={s.button_wrapper}>
-          <Button class={s.button}>开始记账</Button>
+          <RouterLink to="/items/create">
+            <Button class={s.button}>开始记账</Button>
+          </RouterLink>
         </div>
-        <FloatButton iconName="add" />
+        <RouterLink to="/items/create">
+          <FloatButton iconName="add" />
+        </RouterLink>
         {refoverlayVisible.value && (
           <Overlay onClose={() => (refoverlayVisible.value = false)} />
         )}
