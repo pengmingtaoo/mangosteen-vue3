184b6d5da75c9b77a12c6a0b1df072c45756a9a4
pengmingtao <1491614819@qq.com>
2022-10-25 16:09:38 +0800

    完成创建表单并验证功能

--- src/components/tag/TagCreate.tsx
+++ src/components/tag/TagCreate.tsx
@@ -1,40 +1,38 @@
-import { Icon } from "../../shared/Icon";
 import { defineComponent, PropType, reactive } from "vue";
 import { MainLayout } from "../../layouts/MainLayout";
-import s from "./TagCreate.module.scss";
 import { Button } from "../../shared/Button";
 import { EmojiSelect } from "../../shared/EmojiSelect";
+import { Icon } from "../../shared/Icon";
+import { Rules, validate } from "../../shared/validate";
+import s from "./TagCreate.module.scss";
 export const TagCreate = defineComponent({
   props: {
     name: {
       type: String as PropType<string>,
     },
   },
-  setup(props, context) {
-    const formDate = reactive({
+  setup: (props, context) => {
+    const formData = reactive({
       name: "",
       sign: "",
     });
-
+    const errors = reactive<{ [k in keyof typeof formData]?: string[] }>({});
     const onSubmit = (e: Event) => {
-      const rules = [
+      const rules: Rules<typeof formData> = [
+        { key: "name", type: "required", message: "必填" },
         {
           key: "name",
-          Request: true,
-          message: "必填",
+          type: "pattern",
+          regex: /^.{1,4}$/,
+          message: "只能填 1 到 4 个字符",
         },
-        {
-          key: "name",
-          pattern: /^.{1,4}$/,
-          message: "只能填1-4个字符",
-        },
-        {
-          key: "sign",
-          Request: true,
-        },
+        { key: "sign", type: "required", message: "必填" },
       ];
-      const errors = validate(formDate, rules);
-
+      Object.assign(errors, {
+        name: undefined,
+        sign: undefined,
+      });
+      Object.assign(errors, validate(formData, rules));
       e.preventDefault();
     };
     return () => (
@@ -49,31 +47,29 @@ export const TagCreate = defineComponent({
                   <span class={s.formItem_name}>标签名</span>
                   <div class={s.formItem_value}>
                     <input
-                      v-model={formDate.name}
+                      v-model={formData.name}
                       class={[s.formItem, s.input, s.error]}
                     ></input>
                   </div>
                   <div class={s.formItem_errorHint}>
-                    <span>{errors["name"][0]}</span>
+                    <span>{errors["name"] ? errors["name"][0] : "　"}</span>
                   </div>
                 </label>
               </div>
-
               <div class={s.formRow}>
                 <label class={s.formLabel}>
-                  <span class={s.formItem_name}>符号{formDate.sign}</span>
+                  <span class={s.formItem_name}>符号 {formData.sign}</span>
                   <div class={s.formItem_value}>
                     <EmojiSelect
-                      v-model={formDate.sign}
+                      v-model={formData.sign}
                       class={[s.formItem, s.emojiList, s.error]}
                     />
                   </div>
                   <div class={s.formItem_errorHint}>
-                    <span>必填</span>
+                    <span>{errors["sign"] ? errors["sign"][0] : "　"}</span>
                   </div>
                 </label>
               </div>
-
               <p class={s.tips}>记账时长按标签即可进行编辑</p>
               <div class={s.formRow}>
                 <div class={s.formItem_value}>
@@ -87,19 +83,3 @@ export const TagCreate = defineComponent({
     );
   },
 });
-function validate(
-  formDate: { name: string; sign: string },
-  rules: (
-    | { key: string; Request: boolean; message: string; pattern?: undefined }
-    | { key: string; pattern: RegExp; message: string; Request?: undefined }
-    | {
-        key: string;
-        Request: boolean;
-        message?: undefined;
-        pattern?: undefined;
-      }
-  )[]
-) {
-  throw new Error("Function not implemented.");
-}
-
