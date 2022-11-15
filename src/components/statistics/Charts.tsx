89fb5b92a9184a0872d329b0462f02b9dd0470cc
pengmingtao <1491614819@qq.com>
2022-11-15 17:18:09 +0800

    切换 kind 时重新请求数据

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -1,17 +1,17 @@
-import { computed, defineComponent, onMounted, PropType, ref } from "vue";
-import { FormItem } from "../../shared/Form";
-import s from "./Charts.module.scss";
-import { LineChart } from "./LineChart";
-import { PiaChart } from "./PiaChart";
-import { Bar } from "./Bar";
-import { http } from "../../shared/Http";
-import { Time } from "../../shared/time";
+import { computed, defineComponent, onMounted, PropType, ref, watch } from "vue"
+import { FormItem } from "../../shared/Form"
+import s from "./Charts.module.scss"
+import { LineChart } from "./LineChart"
+import { PiaChart } from "./PiaChart"
+import { Bar } from "./Bar"
+import { http } from "../../shared/Http"
+import { Time } from "../../shared/time"
 
-type Data1Item = { happen_at: string; amount: number };
-type Data1 = Data1Item[];
-type Data2Item = { tag_id: number; tag: Tag; amount: number };
-type Data2 = Data2Item[];
-const DAY = 24 * 3600 * 1000;
+type Data1Item = { happen_at: string; amount: number }
+type Data1 = Data1Item[]
+type Data2Item = { tag_id: number; tag: Tag; amount: number }
+type Data2 = Data2Item[]
+const DAY = 24 * 3600 * 1000
 
 export const Charts = defineComponent({
   props: {
@@ -24,28 +24,27 @@ export const Charts = defineComponent({
   },
 
   setup(props, context) {
-    const kind = ref("expenses");
-    const data1 = ref<Data1>([]);
+    const kind = ref("expenses")
+    const data1 = ref<Data1>([])
     const betterData1 = computed<[string, number][]>(() => {
       if (!props.startDate || !props.endDate) {
-        return [];
+        return []
       }
-      const diff = new Date(props.endDate).getTime() - new Date(props.startDate).getTime(); //开始到结束时间挫的差值(总共的秒数)
+      const diff = new Date(props.endDate).getTime() - new Date(props.startDate).getTime() //开始到结束时间挫的差值(总共的秒数)
       //start 1 end 2  n=2
       //start 1 end 3  n=3
-      const n = diff / DAY + 1; //天数
+      const n = diff / DAY + 1 //天数
       return Array.from({ length: n }).map((_, i) => {
-        const time = new Time(props.startDate + "T00:00:00.000+0800").add(i, "day").getTimestamp();
+        const time = new Time(props.startDate + "T00:00:00.000+0800").add(i, "day").getTimestamp()
         //time = 1669737600000（时间搓）,让props.startDate转变成时间搓
         //new Date(time)===Sat Nov 05 2022 00:00:00 GMT+0800 (中国标准时间)
         //new Date(time).toISOString()===2022-10-31T16:00:00.000Z
-        const item = data1.value[0];
-        const amount = item && new Date(item.happen_at).getTime() === time ? data1.value.shift()!.amount : 0;
-        return [new Date(time).toISOString(), amount];
-      });
-    });
-
-    onMounted(async () => {
+        const item = data1.value[0]
+        const amount = item && new Date(item.happen_at).getTime() === time ? data1.value.shift()!.amount : 0
+        return [new Date(time).toISOString(), amount]
+      })
+    })
+    const fetchData1 = async () => {
       const response = await http.get<{ groups: Data1; summary: number }>("/items/summary", {
         happen_after: props.startDate,
         happen_before: props.endDate,
@@ -54,8 +53,10 @@ export const Charts = defineComponent({
         _mock: "itemSummary",
       })
       data1.value = response.data.groups
-    })
-    //
+    }
+    onMounted(fetchData1)
+    watch(() => kind.value, fetchData1)
+    //data2
     const data2 = ref<Data2>([])
     const betterData2 = computed<{ name: string; value: number }[]>(() =>
       data2.value.map((item) => ({
@@ -63,7 +64,7 @@ export const Charts = defineComponent({
         value: item.amount,
       }))
     )
-    onMounted(async () => {
+    const fetchData2 = async () => {
       const response = await http.get<{ groups: Data2; summary: number }>("/items/summary", {
         happen_after: props.startDate,
         happen_before: props.endDate,
@@ -72,8 +73,11 @@ export const Charts = defineComponent({
         _mock: "itemSummary",
       })
       data2.value = response.data.groups
-    })
+    }
+    onMounted(fetchData2)
+    watch(() => kind.value, fetchData2)
 
+    //data3
     const betterData3 = computed<{ tag: Tag; amount: number; percent: number }[]>(() => {
       const total = data2.value.reduce((sum, item) => sum + item.amount, 0)
       return data2.value.map((item) => ({
@@ -100,4 +104,4 @@ export const Charts = defineComponent({
       </div>
     )
   },
-});
+})
--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -10,28 +10,32 @@ export const mockSession: Mock = (config) => {
     jwt: faker.random.word()
   }]
 }
-  let id = 0
-  const createId = () => {
-    id += 1
-    return id
-  }
+let id = 0
+const createId = () => {
+  id += 1
+  return id
+}
 export const mockTagIndex: Mock = (config) => {
-
   const { kind, page } = config.params
   const per_page = 25
   const count = 26
 
-
-  const createPaper = (page = 1) => ({//创建页
-    page, per_page, count
+  const createPaper = (page = 1) => ({
+    //创建页
+    page,
+    per_page,
+    count,
   })
-  const createTag = (n = 1, attrs?: any) =>//创建tag
+  const createTag = (
+    n = 1,
+    attrs?: any //创建tag
+  ) =>
     Array.from({ length: n }).map(() => ({
       id: createId(),
-      name: faker.lorem.word(),//随机
+      name: faker.lorem.word(), //随机
       sign: faker.internet.emoji(),
       kind: config.params.kind,
-      ...attrs
+      ...attrs,
     }))
   const createBody = (n = 1, attrs?: any) => ({
     resources: createTag(n),
@@ -39,85 +43,85 @@ export const mockTagIndex: Mock = (config) => {
     summary: {
       income: 0,
       expenses: 0,
-      balance: 0
-    }
+      balance: 0,
+    },
   })
 
-  if (kind === 'expenses' && (page === 1 || !page)) {
+  if (kind === "expenses" && (page === 1 || !page)) {
     return [200, createBody(25)]
-  } else if (kind === 'expenses' && page === 2) {
+  } else if (kind === "expenses" && page === 2) {
     return [200, createBody(1)]
-  }else if (kind === 'income' && (!page || page === 1)) {
+  } else if (kind === "income" && (!page || page === 1)) {
     return [200, createBody(25)]
-  }else {
-    return [200,createBody(1)]
+  } else {
+    return [200, createBody(1)]
   }
-
 }
-export const mockItemCreate: Mock = config => {
-  return [200, {
-    resource: {
-      "id": 2264,
-      "user_id": 1312,
-      "amount": 9900,
-      "note": null,
-      "tags_id": [3508],
-      "happen_at": "2020-10-29T16:00:00.000Z",
-      "created_at": "2022-07-03T15:35:56.301Z",
-      "updated_at": "2022-07-03T15:35:56.301Z",
-      "kind": "expenses"
-    }
-  }]
-  
+export const mockItemCreate: Mock = (config) => {
+  return [
+    200,
+    {
+      resource: {
+        id: 2264,
+        user_id: 1312,
+        amount: 9900,
+        note: null,
+        tags_id: [3508],
+        happen_at: "2020-10-29T16:00:00.000Z",
+        created_at: "2022-07-03T15:35:56.301Z",
+        updated_at: "2022-07-03T15:35:56.301Z",
+        kind: "expenses",
+      },
+    },
+  ]
 }
-export const mockTagShow: Mock = config => {
-  const createTag = ( attrs?: any) =>//创建tag
-   ({
-      id: createId(),
-      name: faker.lorem.word(),//随机
-      sign: faker.internet.emoji(),
-      kind: 'expenses',
-      ...attrs
-    })
-  return [200, { resource:createTag() }]
+export const mockTagShow: Mock = (config) => {
+  const createTag = (
+    attrs?: any //创建tag
+  ) => ({
+    id: createId(),
+    name: faker.lorem.word(), //随机
+    sign: faker.internet.emoji(),
+    kind: "expenses",
+    ...attrs,
+  })
+  return [200, { resource: createTag() }]
 }
-export const mockTagEdit: Mock = config => {
-  const createTag = (attrs?: any) =>
-    ({
-      id: createId(),
-      name: faker.lorem.word(),
-      sign: faker.internet.emoji(),
-      kind: 'expenses',
-      ...attrs
-    })
-  return [200, {resource: createTag()}]
+export const mockTagEdit: Mock = (config) => {
+  const createTag = (attrs?: any) => ({
+    id: createId(),
+    name: faker.lorem.word(),
+    sign: faker.internet.emoji(),
+    kind: "expenses",
+    ...attrs,
+  })
+  return [200, { resource: createTag() }]
 }
 export const mockItemIndex: Mock = (config) => {
   const { kind, page } = config.params
   const per_page = 25
   const count = 26
-  const createPaper = (page=1) => ({
+  const createPaper = (page = 1) => ({
     page,
     per_page,
-    count
+    count,
   })
-  const createTag = (attrs?: any) =>
-    ({
-      id: createId(),
-      name: faker.lorem.word(),
-      sign: faker.internet.emoji(),
-      kind: 'expenses',
-      ...attrs
-    })
+  const createTag = (attrs?: any) => ({
+    id: createId(),
+    name: faker.lorem.word(),
+    sign: faker.internet.emoji(),
+    kind: "expenses",
+    ...attrs,
+  })
   const createItem = (n = 1, attrs?: any) =>
     Array.from({ length: n }).map(() => ({
       id: createId(),
       user_id: createId(),
       amount: Math.floor(Math.random() * 10000),
       tags_id: [createId()],
-      tags:[createTag()],
+      tags: [createTag()],
       happen_at: faker.date.past().toISOString(),
-      kind:config.params.kind
+      kind: config.params.kind,
     }))
   const createBody = (n = 1, attrs?: any) => ({
     resources: createItem(n),
@@ -132,14 +136,18 @@ export const mockItemIndex: Mock = (config) => {
   }
 }
 export const mockItemIndexBalance: Mock = (config) => {
-  return [200, {
-    expenses: 99000,
-    income: 99000,
-    balance: 0
-  }]
+  return [
+    200,
+    {
+      expenses: 99000,
+      income: 99000,
+      balance: 0,
+    },
+  ]
 }
 export const mockItemSummary: Mock = (config) => {
-  if (config.params.group_by === "happen_at") {
+  const { group_by, kind } = config.params
+  if (group_by === "happen_at" && kind === "expenses") {
     return [
       200,
       {
@@ -151,26 +159,38 @@ export const mockItemSummary: Mock = (config) => {
         summary: 600,
       },
     ]
+  } else if (group_by === "happen_at" && kind === "income") {
+    return [
+      200,
+      {
+        groups: [
+          { happen_at: "2022-11-08T00:00:00.000+0800", amount: 100 },
+          { happen_at: "2022-11-12T00:00:00.000+0800", amount: 800 },
+          { happen_at: "2022-11-19T00:00:00.000+0800", amount: 200 },
+        ],
+        summary: 1100,
+      },
+    ]
+  } else if (group_by === "tag_id" && kind === "income") {
+    return [
+      200,
+      {
+        groups: [
+          { tag_id: 1, tag: { id: 1, name: "交通", sign: faker.internet.emoji() }, amount: 200 },
+          { tag_id: 2, tag: { id: 2, name: "吃饭", sign: faker.internet.emoji() }, amount: 400 },
+          { tag_id: 3, tag: { id: 3, name: "购物", sign: faker.internet.emoji() }, amount: 200 },
+        ],
+        summary: 800,
+      },
+    ]
   } else {
     return [
       200,
       {
         groups: [
-          {
-            tag_id: 1,
-            tag: { id: 1, name: "交通", sign: faker.internet.emoji() },
-            amount: 100,
-          },
-          {
-            tag_id: 2,
-            tag: { id: 2, name: "吃饭", sign: faker.internet.emoji() },
-            amount: 300,
-          },
-          {
-            tag_id: 3,
-            tag: { id: 3, name: "购物", sign: faker.internet.emoji() },
-            amount: 200,
-          },
+          { tag_id: 1, tag: { id: 1, name: "交通", sign: faker.internet.emoji() }, amount: 100 },
+          { tag_id: 2, tag: { id: 2, name: "吃饭", sign: faker.internet.emoji() }, amount: 300 },
+          { tag_id: 3, tag: { id: 3, name: "购物", sign: faker.internet.emoji() }, amount: 200 },
         ],
         summary: 600,
       },
