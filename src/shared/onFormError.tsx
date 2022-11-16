bec1a0d44c195583951ebd126c632d41f30a6307
pengmingtao <1491614819@qq.com>
2022-11-16 19:15:44 +0800

    validate 的&&bug

--- src/shared/onFormError.tsx
+++ src/shared/onFormError.tsx
@@ -1,8 +1,8 @@
 import { AxiosError } from "axios"
-
+//含有语义错误，无法响应。
 export const onFormError =  (error:AxiosError<ResourceError>,fn:(errors:ResourceError)=>void) => {
       if (error.response?.status === 422) {
-       fn(error.response.data)
+        fn(error.response.data)
       }
       throw error
     }
